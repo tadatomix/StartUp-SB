7207191788d55dda0da346c0a6a476caeb754d23
die lin <die lin@PEESEE>
2022-07-07 12:34:32 -0400

    Added more effect

--- Particles.cs
+++ Particles.cs
@@ -54,6 +54,31 @@ namespace StorybrewScripts
                 RadialParticles(237350, 302069);
                 RisingParticles(237350, 302069);
             }
+            using (var pool = new OsbSpritePools(GetLayer("Squares")))
+            {
+                pool.MaxPoolDuration = 300000;
+
+                Action<int, int, double> SquareParticles = (startTime, endTime, maxFade) =>
+                {
+                    for (int i = startTime; i < endTime; i += 40)
+                    {
+                        var duration = Random(2000, 6000);
+                        var fade = Random(0.5f, maxFade);
+                        var pos = new Vector2(Random(-127, 320), 240);
+                        var endPos = new Vector2(Random(320, 767), Random(100, 380));
+
+                        var sprite = pool.Get(i, i + duration, "sb/p.png", OsbOrigin.Centre, false);
+                        sprite.Fade(i, i + 200, 0, fade);
+                        sprite.Move(i, i + duration, pos, endPos);
+                        sprite.Scale(i, Random(2.0f, 8));
+                        sprite.Fade(i + duration - 200, i + duration, fade, 0);
+                        sprite.Rotate(i, i + duration / 2, Random(-Math.PI / 4, Math.PI / 4), Random(-Math.PI / 4, Math.PI / 4));
+                    }
+                };
+                
+                SquareParticles(91732, 102518, 1);
+                SquareParticles(199597, 210384, 0.75);
+            }
         }
     }
 }
